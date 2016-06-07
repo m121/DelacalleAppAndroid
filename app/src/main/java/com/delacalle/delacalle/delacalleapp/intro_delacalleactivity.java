f3dc370099bbfb7ec07195c6125f6e243e559197
m121 <yta123@gmail.com>
2016-06-06 21:43:08 -0500

    fotos eliminadas

--- app/src/main/java/com/delacalle/delacalle/delacalleapp/intro_delacalleactivity.java
+++ app/src/main/java/com/delacalle/delacalle/delacalleapp/intro_delacalleactivity.java
@@ -7,6 +7,8 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,53 +18,60 @@ import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class intro_delacalleactivity extends AppIntro2 {
 
-    // Please DO NOT override onCreate. Use init.
-    @Override
-    public void init(Bundle savedInstanceState) {
 
-        SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
-        if(pref.getBoolean("activity_executed", false)){
-            Intent intent = new Intent(this, iniciosesion_delacalleactivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            SharedPreferences.Editor ed = pref.edit();
-            ed.putBoolean("activity_executed", true);
-            ed.commit();
-        }
+        // Please DO NOT override onCreate. Use init.
+        @Override
+        public void init (Bundle savedInstanceState){
+try {
+    SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
+    if (pref.getBoolean("activity_executed", false)) {
+        Intent intent = new Intent(this, iniciosesion_delacalleactivity.class);
+        startActivity(intent);
+        finish();
+    } else {
+        SharedPreferences.Editor ed = pref.edit();
+        ed.putBoolean("activity_executed", true);
+        ed.commit();
+    }
 
 
-        // Add your slide's fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide(AppIntroFragment.newInstance("Tutorial", "Te explicamos como usar el club de la calle", R.drawable.tuto,  Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("Contacto", "Contactános por correo, por Whatsapp, por Facebook o por delacalle.co", R.drawable.tuto1, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("¿Cuanto cuesta?", "Pagas solamente $5.000 pesos por un mes", R.drawable.tuto3, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("¿Que beneficios obtienes?", "¡Todo el mes recibes descuentos de nuestros establecimientos aliados! ", R.drawable.tuto4, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("", "", R.drawable.tuto5, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("¿Como es la membresía?", "La membresía es una tarjeta con el logo del club De La Calle ", R.drawable.tuto6, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("¿Como buscas ofertas?", "Puedes buscar las ofertas usando nuestra app o visitando la web delacalle.co ", R.drawable.tuto7, Color.rgb(26,26,26)));
-        addSlide(AppIntroFragment.newInstance("Presentar membresía", "Ve a un establecimiento, presentas la membresía delacalle y obtendrás tu descuento.¡Hazlo ya! ", R.drawable.tuto8, Color.rgb(26,26,26)));
-  //      addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
-      //  addSlide(third_fragment);
-     //   addSlide(fourth_fragment);
+    // Add your slide's fragments here.
+    // AppIntro will automatically generate the dots indicator and buttons.
+    addSlide(AppIntroFragment.newInstance("Tutorial", "Te explicamos como usar el club de la calle", R.drawable.tuto, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("Contacto", "Contactános por correo, por Whatsapp, por Facebook o por delacalle.co", R.drawable.tuto1, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("¿Cuanto cuesta?", "Pagas solamente $5.000 pesos por un mes", R.drawable.tuto3, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("¿Que beneficios obtienes?", "¡Todo el mes recibes descuentos de nuestros establecimientos aliados! ", R.drawable.tuto4, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("", "", R.drawable.tuto5, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("¿Cómo es la membresía?", "La membresía es una tarjeta con el logo del club De La Calle ", R.drawable.tuto6, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("¿Cómo buscas ofertas?", "Puedes buscar las ofertas usando nuestra app o visitando la web delacalle.co ", R.drawable.tuto7, Color.rgb(26, 26, 26)));
+    addSlide(AppIntroFragment.newInstance("Presentar membresía", "Ve a un establecimiento, presentas la membresía delacalle y obtendrás tu descuento.¡Hazlo ya! ", R.drawable.tuto8, Color.rgb(26, 26, 26)));
+    //      addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+    //  addSlide(third_fragment);
+    //   addSlide(fourth_fragment);
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-   //     addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+    // Instead of fragments, you can also use our default slide
+    // Just set a title, description, background and image. AppIntro will do the rest.
+    //     addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
 
-        // OPTIONAL METHODS
-        // Override bar/separator color.
-     //   setBarColor(Color.parseColor("#3F51B5"));
-       // setSeparatorColor(Color.parseColor("#2196F3"));
+    // OPTIONAL METHODS
+    // Override bar/separator color.
+    //   setBarColor(Color.parseColor("#3F51B5"));
+    // setSeparatorColor(Color.parseColor("#2196F3"));
 
-        // Hide Skip/Done button.
-      //  showSkipButton(true);
-        setProgressButtonEnabled(true);
+    // Hide Skip/Done button.
+    //  showSkipButton(true);
+    setProgressButtonEnabled(true);
 
-        // Turn vibration on and set intensity.
-        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
-        setVibrate(false);
-        setVibrateIntensity(30);
+    // Turn vibration on and set intensity.
+    // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+    setVibrate(false);
+    setVibrateIntensity(30);
+}catch (Exception ae)
+{
+    Log.d("delacalle","Error en el intro");
+    Intent intent = new Intent(intro_delacalleactivity.this,iniciosesion_delacalleactivity.class);
+    startActivity(intent);
+}
     }
 
    /* @Override
